a422c458120da513706111ed6a2920c1fdc43f47
Denzol255 <den-zoloto@mail.ru>
2021-10-07 18:14:06 +0300

    Added: header mobile and PC, some styles for mobile menu and search

--- src/js/modules/clickListner.js
+++ src/js/modules/clickListner.js
@@ -9,9 +9,16 @@ const clickListner = () => {
         target.matches('.header__burger') ||
         target.closest('.header__burger')
       ) {
-        document.querySelector('.menu').classList.toggle('_active');
         document.querySelector('.header__burger').classList.toggle('_active');
-        document.body.classList.toggle('_lock');
+        document
+          .querySelector('.header__menu.menu')
+          .classList.toggle('_active');
+      }
+      if (
+        target.matches('.search-header__icon') ||
+        target.closest('.search-header__icon')
+      ) {
+        document.querySelector('.search-header').classList.toggle('_active');
       }
       if (window.innerWidth > 768 && isMobile.any()) {
         if (target.matches('.menu__arrow')) {
